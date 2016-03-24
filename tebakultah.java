c2d983a611b9fcc2dd711b6db6795b618466e822
Ibnu Syarif Al-Husein <kanghusein.future@gmail.com>
2016-03-24 17:35:47 +0700

    Menambahkan pertanyaan dan perhitungan

--- tebakultah.java
+++ tebakultah.java
@@ -31,5 +31,58 @@ public class tebakultah{
         "20 21 22 23\n" +
         "24 25 26 27\n" +
         "28 29 30 31";
+        
+        int hari = 0;
+        
+        // Menciptakan Scanner
+        Scanner masukan = new Scanner(System.in);
+        
+        // Meminta pengguna untuk memberikan jawaban
+        System.out.print("Apakah Ultah Anda dalam set1?\n");
+        System.out.print(set1);
+        System.out.print("\nMasukkan 0 bila Tidak dan 1 bila Ya: ");
+        int jawab = masukan.nextInt();
+        
+        if (jawab == 1)
+            hari += 1;
+        
+        // Meminta pengguna untuk memberikan jawaban
+        System.out.print("Apakah Ultah Anda dalam set2?\n");
+        System.out.print(set2);
+        System.out.print("\nMasukkan 0 bila Tidak dan 1 bila Ya: ");
+        int jawab2 = masukan.nextInt();
+        
+        if (jawab2 == 1)
+            hari += 2;
+
+        // Meminta pengguna untuk memberikan jawaban
+        System.out.print("Apakah Ultah Anda dalam set3?\n");
+        System.out.print(set3);
+        System.out.print("\nMasukkan 0 bila Tidak dan 1 bila Ya: ");
+        int jawab3 = masukan.nextInt();
+        
+        if (jawab3 == 1)
+            hari += 4;
+
+        // Meminta pengguna untuk memberikan jawaban
+        System.out.print("Apakah Ultah Anda dalam set4?\n");
+        System.out.print(set4);
+        System.out.print("\nMasukkan 0 bila Tidak dan 1 bila Ya: ");
+        int jawab4 = masukan.nextInt();
+        
+        if (jawab4 == 1)
+            hari += 8;
+
+        // Meminta pengguna untuk memberikan jawaban
+        System.out.print("Apakah Ultah Anda dalam set5?\n");
+        System.out.print(set5);
+        System.out.print("\nMasukkan 0 bila Tidak dan 1 bila Ya: ");
+        int jawab5 = masukan.nextInt();
+        
+        if (jawab5 == 1)
+            hari += 16;
+            
+        // Tampilkan hasil perhitungan
+        System.out.println("\nHari Ulang Tahun Anda adalah " + hari + "!");
     }
 }
